716f864b079b6746f450f2c3085afc4647f71d15
sofiasajid <sofiasajid8372@gmail.com>
2014-04-25 17:01:21 +0500

    matrix inverse calculations

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat=matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    mat <<- y
+    inv <<- NULL
+    print(flag)
+  }
+  get <- function() mat
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() 
+    inv
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  if(nrow(data)==ncol(data) && det(data)!=0){
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv}
+  
+  else print("inverse is not possible as determinant is zero")
+    
+  
 }
