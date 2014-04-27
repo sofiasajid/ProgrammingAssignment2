e5557cfb41cad3a41a64a8f049c821f8cafc33ff
sofiasajid <sofiasajid8372@gmail.com>
2014-04-27 23:43:52 +0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions computes inverse of invertible matrix 
+## by taking a matrix as an input from the user
 
-## Write a short comment describing this function
+## this function accepts a matrix as an input from the user 
+##and set the value of cache to inverse of matrix
 
 makeCacheMatrix <- function(mat=matrix()) {
   inv <- NULL
-  set <- function(y) {
+  set <- function(y) {                                    # set a value of matrix
     mat <<- y
     inv <<- NULL
-    print(flag)
   }
-  get <- function() mat
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() 
+  get <- function() mat                                  #  Get a value of matrix
+  setinverse <- function(inverse) inv <<- inverse        #  set a cache to inverse of matrix  
+  getinverse <- function()                               #  get a inverse matrix
     inv
   
-  list(set = set, get = get,
+  list(set = set, get = get,                              # function returns a list so that user can set and get a matrix and so its inverse dynamically
        setinverse = setinverse,
        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##  This function cehecks the cache for inverse or to calculate the inverse after validating the matrix for inverse calculation
 
 cacheSolve<- function(x, ...) {
   inv <- x$getinverse()
@@ -30,12 +30,12 @@ cacheSolve<- function(x, ...) {
     return(inv)
   }
   data <- x$get()
-  if(nrow(data)==ncol(data) && det(data)!=0){
+  if(nrow(data)==ncol(data) && det(data)!=0){   # checks for the square matrix and "0" determinant(valid input)
     inv <- solve(data, ...)
     x$setinverse(inv)
     inv}
   
-  else print("inverse is not possible as determinant is zero")
+  else print("inverse is not possible as determinant is zero or mattrix is not square matrix")
     
   
 }
